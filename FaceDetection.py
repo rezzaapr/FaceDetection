a4ce9465c511893e2f64133d328392a0ce52f252
Rezza Priatna <58212770+rezzaapr@users.noreply.github.com>
2020-10-10 15:54:25 +0700

    Update FaceDetection.py

--- FaceDetection.py
+++ FaceDetection.py
@@ -7,23 +7,20 @@ face_cascade = cv2.CascadeClassifier('cascade/haarcascade_frontalface_default.xm
 smile_cascade = cv2.CascadeClassifier('cascade/haarcascade_smile.xml')
 eye_cascade = cv2.CascadeClassifier('cascade/haarcascade_eye.xml')
 
-# Your Ip Webcam Addres
+# IP webcam 
 url='http://192.168.43.1:8080/shot.jpg' 
 
 while True:
-    imgResp = urlopen(url) #GET Image From IPWebcam
-    imgNp = np.array(bytearray(imgResp.read()),dtype=np.uint8) # Convert Image To Array
-    img = cv2.imdecode(imgNp,-1) # Image Results
-    ### FACE DETECTION ###
+    imgResp = urlopen(url) 
+    imgNp = np.array(bytearray(imgResp.read()),dtype=np.uint8)
+    img = cv2.imdecode(imgNp,-1)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
     faces = face_cascade.detectMultiScale(gray, 1.1, 4)
     for (x, y, w, h) in faces:
        img = cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
-    ### SMILE DETECTION ###
     smiles  = smile_cascade.detectMultiScale(img, scaleFactor = 1.8, minNeighbors = 20)
     for (sx, sy, sw, sh) in smiles:
        img = cv2.rectangle(img, (sx, sy), ((sx + sw), (sy + sh)), (0, 255,0), 2)
-    ### EYE DETECTION ###
     eyes = eye_cascade.detectMultiScale(img, scaleFactor = 1.1, minNeighbors = 5)
     for (ex,ey,ew,eh) in eyes:
        img = cv2.rectangle(img,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
